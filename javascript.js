95745f79b93030b389dfa56e15695df88f3b2eac
Thegamefire <60431842+Thegamefire@users.noreply.github.com>
2020-09-06 17:00:55 +0200

    AmongUs

--- javascript.js
+++ javascript.js
@@ -2,4 +2,8 @@ let redCode = prompt("Geef je code in");
 
 if (redCode === "Boek3") {
 	window.location = "https://mega.nz/folder/AmJFGYAJ#LjIJCrlDPEfPFcaqStE_xg"
+} else {
+	if (redCode === "AmongUs") {
+		window.location = "OnderOns.exe"
+	}
 }
